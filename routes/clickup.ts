fa5a45098e56411cbb174b5daa7d49e16611e7ea
mikehttt <mike.hoang@ttt.studio>
2024-11-12 13:40:34 -0800

    feat: Add endpoint to fetch tasks from ClickUp

--- routes/clickup.ts
+++ routes/clickup.ts
@@ -74,6 +74,54 @@ clickupRouter.get('/task/:workspaceId/:taskId', async (c) => {
   }
 });
 
+clickupRouter.post('/tasks/:workspaceId', async (c) => {
+  const workspaceId = c.req.param('workspaceId');
+  const clickUpToken = c.get('clickUpToken');
+  const tasks = await c.req.json<{ id: string; custom_task_ids: boolean }[]>();
+
+  console.log({ tasks });
+
+  if (!workspaceId) {
+    return c.json({ error: 'Missing workspaceId parameter' }, 400);
+  }
+
+  if (!tasks || !tasks.length) {
+    return c.json(
+      { error: 'Missing or empty tasks array in request body' },
+      400
+    );
+  }
+
+  try {
+    const results = await Promise.all(
+      tasks.map(async (task) => {
+        const response = await fetch(
+          `${CLICKUP_BASE_API}/v2/task/${task.id}?team_id=${workspaceId}&custom_task_ids=${task.custom_task_ids}&include_markdown_description=true`,
+          {
+            headers: {
+              Authorization: clickUpToken,
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          return { error: errorData, status: response.status, id: task.id };
+        }
+
+        const data = await response.json();
+        return { ...data, id: task.id };
+      })
+    );
+
+    return c.json({ tasks: results });
+  } catch (error) {
+    console.error('Error fetching tasks from ClickUp:', error);
+    return c.json({ error: 'Failed to fetch data from ClickUp' }, 500);
+  }
+});
+
 clickupRouter.get('/workspaces/:workspaceId/docs/:docId', async (c) => {
   const workspaceId = c.req.param('workspaceId');
   const docId = c.req.param('docId');
